8410195e7c19fa3a8fc38e4b39da4bf04a3a9446
Kirill Vasko <kirill.vasko.2003@gmail.com>
2024-04-22 17:33:07 +0300

    build: install dependencies, config project and add postcss, alias, stylelint;

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      assets: '/src/assets',
+      components: '/src/components',
+      constants: '/src/constants',
+      modules: '/src/modules',
+      pages: '/src/pages',
+      utils: '/src/utils',
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+});
