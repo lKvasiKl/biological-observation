d4b408b271b9b459d9d5e6177fcfc75f9ffa08fd
Kirill Vasko <kirill.vasko.2003@gmail.com>
2024-04-23 23:52:35 +0300

    feat: biom data mapper;

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,6 @@ import biomObject from 'data/biom.json';
 
 export const App = () => {
   const data = mapBiomToBacteriaRows(biomObject);
-
   console.log(data);
 
   return <div>Biological observation</div>;
